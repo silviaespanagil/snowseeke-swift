59688ab847865c8c313bedd9886913d5b27a3dd9
Silvia España <silviaespanagil@gmail.com>
2021-10-26 15:57:25 +0200

    first commit

---
+++ snowseek/snowseekApp.swift
@@ -0,0 +1,17 @@
+//
+//  snowseekApp.swift
+//  snowseek
+//
+//  Created by Silvia España on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct snowseekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
